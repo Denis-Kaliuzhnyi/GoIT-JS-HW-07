5d5f40319451187207634cee3a147a47d863e675
Denis Kaliuzhnyi <deniskl4200@gmail.com>
2024-03-30 15:21:26 +0200

    task-2.2

--- js/task-2.js
+++ js/task-2.js
@@ -11,34 +11,33 @@ const images = [
     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Group of Horses Running',
   },
-  {
-    url:
-      'https://images.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    alt: 'Elephant Beside on Baby Elephant',
-  },
-  {
-    url:
-      'https://images.pexels.com/photos/37833/rainbow-lorikeet-parrots-australia-rainbow-37833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    alt: 'Blue Geeen and Orange Parrot',
-  },
-  {
-    url:
-      'https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    alt: 'Zebras on Zebra',
-  },
+  // {
+  //   url:
+  //     'https://images.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+  //   alt: 'Elephant Beside on Baby Elephant',
+  // },
+  // {
+  //   url:
+  //     'https://images.pexels.com/photos/37833/rainbow-lorikeet-parrots-australia-rainbow-37833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+  //   alt: 'Blue Geeen and Orange Parrot',
+  // },
+  // {
+  //   url:
+  //     'https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+  //   alt: 'Zebras on Zebra',
+  // },
 ];
 
 const imagesGallery = document.querySelector('.gallery');
-const elementsLi = document.createElement('li');
-elementsLi.classList.add('imagesGallery');
-const elemA = document.createElement ('a');
-const image = document.createElement ('img');
-image.src = 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
-image.alt = 'White and Black Long Fur Cat'
 
+images.forEach(image => {
 
+const elLi = document.createElement('li');
+const elImage = document.createElement('img');
 
-// Використовуй масив об'єктів images для створення елементів <img>, вкладених в <li>.
-// Ти можеш створити й додати HTML-елементи, використовуючи document.createElement() і elem.append() або шаблонні рядки і elem.insertAdjacentHTML().
-// Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
-// Додай мінімальне оформлення галереї флексбоксами через CSS класи.
+elImage.src = image.url;
+elImage.alt = image.alt;
+
+elLi.appendChild(elImage);
+imagesGallery.appendChild(elLi)})
+
