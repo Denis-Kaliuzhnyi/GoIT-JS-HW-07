fb01d56f60704895ad4e3c5e9a219056f83b37f5
Denis Kaliuzhnyi <deniskl4200@gmail.com>
2024-03-31 19:43:34 +0300

    task-6

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const body = document.body;
+const btn = document.querySelector(".change-color");
+const spanColor = document.querySelector(".color");
+
+btn.addEventListener("click", () => {
+  const newRandomColor = getRandomHexColor();
+  body.style.backgroundColor = newRandomColor;
+  spanColor.textContent = newRandomColor;
+});
+
