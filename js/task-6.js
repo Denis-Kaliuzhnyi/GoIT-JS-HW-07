ad0ade171246fe98ff74d4e957c020342684220a
Denis Kaliuzhnyi <deniskl4200@gmail.com>
2024-03-31 20:15:58 +0300

    task-6

--- js/task-6.js
+++ js/task-6.js
@@ -4,43 +4,40 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const inputEl = document.querySelector(".input");
+const bxContainer = document.getElementById("boxes");
 
-// Напиши скрипт створення й очищення колекції елементів з наступним функціоналом.
+function createBoxes(amount) {
+  bxContainer.innerHTML = "";
 
-// Є input, у який користувач вводить бажану кількість елементів. Після натискання на кнопку Create має рендеритися (додаватися в DOM) колекція з відповідною кількістю елементів і очищатися значення в інпуті. При повторному натисканні на кнопку Create поверх старої колекції має рендеритись нова. Після натискання на кнопку Destroy колекція елементів має очищатися.
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    bxContainer.appendChild(box);
+    size += 10;
+  }
+}
+function destroyBoxes() {
+  bxContainer.innerHTML = "";
+}
 
+createBtn.addEventListener("click", () => {
+  const number = parseInt(inputEl.value);
+  if (number >= 1 && number <= 100) {
+    createBoxes(number);
+    inputEl.value = "";
+  } else {
+    alert ("Please enter a number between 1 and 100.");
+  }
+});
 
-// <div id="controls">
-//   <input type="number" min="1" max="100" step="1" />
-//   <button type="button" data-create>Create</button>
-//   <button type="button" data-destroy>Destroy</button>
-// </div>
+destroyBtn.addEventListener("click", () => {
+  destroyBoxes();
+});
 
-// <div id="boxes"></div>
-
-// Після натискання користувачем на кнопку Create треба провалідувати значення в input, воно має бути в межах від 1 до 100 включно. Тільки якщо воно задоволяє умову, мають додаватися нові <div> елементи в DOM.
-
-// Для рендеру елементів на сторінці створи функцію createBoxes(amount), яка приймає один параметр — число, що зберігає кількість елементів для рендеру.
-
-// Функція має створювати стільки <div> елементів, скільки вказано в параметрі amount і додавати їх у DOM дочірніми елементами для div#boxes.
-
-// Розміри першого <div> елемента мають бути 30px на 30px.
-// Кожен наступний елемент повинен бути ширшим і вищим від попереднього на 10px.
-// Усі елементи повинні мати випадковий колір фону. Використовуй готову функцію getRandomHexColor() для отримання випадкового кольору.
-
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
-
-// Для очищення колекції після натискання на кнопку Destroy створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб видаляючи всі створені елементи.
-
-// На що буде звертати увагу ментор при перевірці:
-
-// Після кліку на кнопку Create, якщо в input значення поза межами діапазону 1-100, нічого не відбувається
-// Після кліку на кнопку Create в div#boxes додається така кількість різнокольорових квадратів, яка вказана в input. Значення input очищається
-// Після повторного кліку на кнопку Create попередні квадрати повністю прибираються і замість них додаються нові у кількості, що вказана в input. Значення input очищається
-// Усі квадрати в div#boxes різнокольорові і мають фон випадкового кольору
-// Перший квадрат у div#boxes має розміри 30px на 30px, а кожен наступний на 10px вищий і ширший від попереднього
-// Після натискання на кнопку Destroy усі квадрати з div#boxes мають видалятися
